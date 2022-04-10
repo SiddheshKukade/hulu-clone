0a38ec03d7138be08b91eb3ac977006781558e8f
SiddheshKukade <littlelord124@gmail.com>
2022-04-10 14:20:44 +0530

    📦 NEW: Header Done

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Header from './../components/Header/Header';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-    {/*  */}
+      <Head>
+        <title>HULU - A place to relax</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Header  */}
+      <Header />
+      {/* Nav */}
+      {/* Reuslts */}
     </div>
   )
 }
