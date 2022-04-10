d22ae79d6e67b326da630dcece6bfb5de478fce6
SiddheshKukade <littlelord124@gmail.com>
2022-04-10 17:21:13 +0530

    📦 NEW: Project Done

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -4,11 +4,11 @@ const Navbar = () => {
     const router = useRouter()
     return (
         <nav className="relative">
-            <div className="scrollbar-hide flex px-10  sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll ">
+            <div className="scrollbar-hide flex px-10  sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll pb-4 ">
                 {Object.entries(requests).map(([key, { title, url }]) => (
-                    <h2 onClick={() => router.push(`/?genre=${key}`)} key={key} className='cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white last:pr-24  '>
+                    <button onClick={() => router.push(`/?genre=${key}`)} key={key} className='cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white last:pr-24 focus:font-bold z-10  focus:scale-125 active:scale-125 '>
                         {title}
-                    </h2>))}
+                    </button >))}
             </div>
             <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"></div>
         </nav>
--- components/Thumbnail/Thumbnail.js
+++ components/Thumbnail/Thumbnail.js
@@ -1,11 +1,12 @@
 import { ThumbUpIcon } from '@heroicons/react/outline';
+import { forwardRef } from 'react';
 import Image from 'next/image';
-const Thumbnail = ({ result }) => {
+const Thumbnail = forwardRef(({ result }, ref) => {
     const BASE_URL = 'https://image.tmdb.org/t/p/original/';
     return (
-        <div className='group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
+        <div ref={ref} className='group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
             <Image layout='responsive'
-                src={`${BASE_URL}${result.backdrop_path || result.poster.path}` || ` ${BASE_URL}${reuslt.poster_path}`} height={1080} width={1920} />
+                src={`${BASE_URL}${result ?  .backdrop_path || result.poster?.path}` || ` ${BASE_URL}${reuslt.poster_path}`} height={1080} width={1920} />
             <div className='p-2'>
                 <p className='truncate max-w-md'>{result.overview}</p>
                 <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>{result.title || result.original_name}</h2>
@@ -15,6 +16,6 @@ const Thumbnail = ({ result }) => {
             </div>
         </div>
     )
-}
+})
 
 export default Thumbnail
